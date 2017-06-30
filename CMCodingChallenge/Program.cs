cc29905771b5548cbeddab958969582fb34d1f4d
pbar1 <pbar1@users.noreply.github.com>
2017-06-30 12:51:30 -0500

    Add project files.

---
+++ CMCodingChallenge/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fizz;
+
+namespace CMCodingChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Default FizzBuzzer ===");
+            var defaultBuzzer = new FizzBuzzer();
+            foreach (var s in defaultBuzzer.Run())
+                Console.WriteLine(s);
+
+            Console.WriteLine("\n=== Default FizzBuzzer, updated parameters ===");
+            var updatedDivisors = new Dictionary<int, string>();
+            updatedDivisors.Add(10, "Taco");
+            defaultBuzzer.SetDivisors(updatedDivisors);
+            defaultBuzzer.SetUpperBound(40);
+            foreach (var s in defaultBuzzer.Run())
+                Console.WriteLine(s);
+
+            Console.WriteLine("\n=== New FizzBuzzer, all new parameters ===");
+            var newDivisors = new Dictionary<int, string>();
+            newDivisors.Add(4, "Ash");
+            newDivisors.Add(7, "Brock");
+            newDivisors.Add(8, "Misty");
+            var newBuzzer = new FizzBuzzer(newDivisors, 56);
+            foreach (var s in newBuzzer.Run())
+                Console.WriteLine(s);
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzzer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzer
+    {
+        private Dictionary<int, string> _divisors;
+        private int _upperBound;
+
+        public FizzBuzzer()
+        {
+            Dictionary<int, string> divisors = new Dictionary<int, string>();
+            divisors.Add(3, "Fizz");
+            divisors.Add(5, "Buzz");
+            SetUpperBound(100);
+        }
+
+        public FizzBuzzer(Dictionary<int, string> divisors, int upperBound)
+        {
+            SetDivisors(divisors);
+            SetUpperBound(upperBound);
+        }
+
+        public void SetDivisors(Dictionary<int, string> divisors)
+        {
+            _divisors = divisors;
+        }
+
+        public void SetUpperBound(int upperBound)
+        {
+            _upperBound = upperBound;
+        }
+
+        public List<string> Run()
+        {
+            List<string> results = new List<string>();
+            for (int i = 1; i <= _upperBound; i++)
+            {
+                List<string> tempList = new List<string>();
+                foreach (var d in _divisors)
+                    if (i % d.Key == 0)
+                        tempList.Add(d.Value);
+                if (tempList.Count == 0)
+                    results.Add(i.ToString());
+                else if (tempList.Count == 1)
+                    results.Add(tempList[1]);
+                else
+                    results.Add( string.Join(", ", tempList.ToArray()) );
+            }
+            return results;
+        }
+    }
+}
